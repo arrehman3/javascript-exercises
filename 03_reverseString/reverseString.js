a26254f74663dd41f728a5ffbfa67ef4748cf5b1
arrehman3 <abdurrehman988.ar@gmail.com>
2024-06-30 02:41:28 +0500

    task 3,4

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let newString='';
+    let lengthOfTheString = string.length-1;
+    for(i=lengthOfTheString;i>=0;i--){
+        let theCharacter=string.charAt(i);
+        newString=newString.concat(theCharacter);
+    }
 
+    return newString;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,...item) {
+        let newArray =  array.filter(element=>!item.includes(element));
+        return newArray;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
