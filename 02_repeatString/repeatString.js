c389f6198c07934737449aba9a4ab2d808fb1f55
arrehman3 <abdurrehman988.ar@gmail.com>
2024-06-30 01:59:47 +0500

    task1 and task2 completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
 
+const repeatString = function(string,numberOfTimes) {
+    let newString='';
+    if(numberOfTimes<0){
+        return `ERROR`;
+    }
+    for(i=1;i<=numberOfTimes;i++){
+        newString=newString.concat(string);
+        
+    }
+
+    return newString;
 };
 
 // Do not edit below this line
