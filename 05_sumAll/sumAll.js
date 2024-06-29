be007f3a85005c71fd8161927e67efe4a633d982
arrehman3 <abdurrehman988.ar@gmail.com>
2024-06-30 03:37:24 +0500

    tasks 5,6,7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function(startingNumber,endingNumber) {
+    if(!Number.isInteger(startingNumber) || !Number.isInteger(endingNumber) ){
+        return 'ERROR';
+    }
+    if (startingNumber<0 || endingNumber<0) return 'ERROR';
+    let totalSum = 0;
+    if(startingNumber<endingNumber){
+        
+        for(i=startingNumber;i<=endingNumber;i++){
+            totalSum+=i;
+        }
 
+        return totalSum;
+    }
+    else if(startingNumber>endingNumber){
+        for(i=endingNumber;i<=startingNumber;i++){
+            totalSum+=i;
+        }
+
+        return totalSum;
+
+       
+    }
+    
+
+    
 };
 
 // Do not edit below this line
