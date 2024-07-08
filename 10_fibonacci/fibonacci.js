bc8929eddce61ba62b8c44ca252068b5a3834c1d
arrehman3 <abdurrehman988.ar@gmail.com>
2024-07-09 01:56:12 +0500

    only plaindrome left

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if(n<0){
+        return 'OOPS';
+    }
+    else if(n==0){
+        return 0;
+    }
+    else if (n==1){
+        return 1
+    }
+    else if(n==2){
+        return 1;
+    }
+    else{
+        return fibonacci(n-1)+fibonacci(n-2);
+    }
 
 };
 
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest,currentP)=>{
+        const oldestAge = getAge(oldest.yearOfBirth,oldest.yearOfDeath);
+        const currentAge =getAge(currentP.yearOfBirth,currentP.yearOfDeath);
+        return oldestAge>currentAge? oldest:currentP;
+        });
 };
 
+const getAge = function(birth,death){
+    if(!death){
+        death = new Date().getFullYear();
+
+    }
+
+    return death-birth;
+}
 // Do not edit below this line
 module.exports = findTheOldest;
